88e29a3c1b3b62e85eb16b57371f95dacbb17904
Yuqi <yuqiliuxjtu@gmail.com>
2015-09-18 23:09:58 -0400

    solution05

--- ch4/Solution05.java
+++ ch4/Solution05.java
@@ -1,14 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public boolean isValidBST(TreeNode root) {
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution05 {
+    public static boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
         }
@@ -27,12 +27,27 @@ public class Solution {
     }
     
     
-    public void inorder(ArrayList<Integer> list, TreeNode root) {
+    public static void inorder(ArrayList<Integer> list, TreeNode root) {
         if (root == null) {
             return;
         }
         inorder(list, root.left);
         list.add(root.val);
         inorder(list, root.right);
+    }
+
+    public static void main(String[] args) {
+    	TreeNode[] nodes = new TreeNode[7];
+    	for (int i = 0; i < 7; i ++) {
+    		nodes[i] = new TreeNode(i);
+    	}
+
+    	nodes[3].left = nodes[2];
+    	nodes[2].left = nodes[1];
+    	nodes[3].right = nodes[5];
+    	nodes[5].left = nodes[4];
+    	nodes[5].right = nodes[6];
+
+    	System.out.println((isValidBST(nodes[3]) == true)? "true": "false");
     }
 }
